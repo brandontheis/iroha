1d25f8bcc39d80530fc6d1313c2d6485a26c6dc8
kamilsa <kamilsa16@gmail.com>
2017-09-11 10:10:36 +0300

    Amount stubs, add boost

--- libs/amount/amount.cpp
+++ libs/amount/amount.cpp
@@ -15,131 +15,32 @@
  * limitations under the License.
  */
 
-#include <utility>
-
 #include "amount/amount.hpp"
 
-namespace iroha {
+namespace amount {
 
-  // to raise to power integer values
-  int ipow(int base, int exp) {
-    int result = 1;
-    while (exp != 0) {
-      if (exp & 1) result *= base;
-      exp >>= 1;
-      base *= base;
-    }
+  Amount::Amount() {}
 
-    return result;
+  Amount::Amount(uint64_t amount, uint8_t precision) {
+    value = amount;
+    value /= std::pow(10, precision);
   }
-
-  Amount::Amount() : value_(0), precision_(0) {}
-
-  Amount::Amount(uint256_t value) : value_(value), precision_(0) {}
-
-  Amount::Amount(uint256_t amount, uint8_t precision)
-      : value_(amount), precision_(precision) {}
-
-  Amount::Amount(const Amount &am)
-      : value_(am.value_), precision_(am.precision_) {}
-
-  Amount &Amount::operator=(const Amount &other) {
-    // check for self-assignment
-    if (&other == this) return *this;
-    value_ = other.value_;
-    precision_ = other.precision_;
-    return *this;
-  }
-
-  Amount::Amount(Amount &&am) : value_(am.value_), precision_(am.precision_) {}
-
-  Amount &Amount::operator=(Amount &&other) {
-    std::swap(value_, other.value_);
-    std::swap(precision_, other.precision_);
-    return *this;
-  }
-
-  Amount Amount::percentage(uint256_t percents) const {
-    uint256_t new_val = value_ * percents / 100;
-    return {new_val, precision_};
-  }
-
-  Amount Amount::percentage(const Amount &am) const {
-    // multiply two amount values
-    uint256_t new_value = value_ * am.value_;
-
-    // new value should be decreased by the scale of am to move floating point
-    // to the left, as it is done when we multiply manually
-    new_value /= uint256_t(pow(10, am.precision_));
-    // to take percentage value we need divide by 100
-    new_value /= 100;
-    return {new_value, precision_};
-  }
-
-  Amount Amount::add(const Amount &other) const {
-    auto new_val = value_ + other.value_;
-    return {new_val, precision_};
-  }
-
-  Amount &Amount::operator+=(const Amount &other) {
-    if (precision_ != other.precision_) {
-      throw std::invalid_argument("precisions are not the same");
-    }
-    value_ += other.value_;
-    return *this;
-  }
-
-  Amount Amount::subtract(const Amount &other) const {
-    auto new_val = value_ - other.value_;
-    return {new_val, precision_};
-  }
-
-  Amount &Amount::operator-=(const Amount &other) {
-    if (precision_ != other.precision_) {
-      throw std::invalid_argument("precisions are not the same");
-    }
-    value_ -= other.value_;
-    return *this;
-  }
-
-  int Amount::compareTo(const Amount &other) const {
-    if (precision_ == other.precision_) {
-      return (value_ < other.value_) ? -1 : (value_ > other.value_) ? 1 : 0;
-    }
-    // when different precisions transform to have the same scale
-    auto max_precision = std::max(precision_, other.precision_);
-    auto val1 = value_ * ipow(10, max_precision - precision_);
-    auto val2 = other.value_ * ipow(10, max_precision - other.precision_);
-    return (val1 < val2) ? -1 : (val1 > val2) ? 1 : 0;
-  }
-
-  bool Amount::operator==(const Amount &other) const {
-    return compareTo(other) == 0;
-  }
-
-  bool Amount::operator!=(const Amount &other) const {
-    return compareTo(other) != 0;
-  }
-
-  bool Amount::operator<(const Amount &other) const {
-    return compareTo(other) < 0;
-  }
-
-  bool Amount::operator>(const Amount &other) const {
-    return compareTo(other) > 0;
-  }
-
-  bool Amount::operator<=(const Amount &other) const {
-    return compareTo(other) <= 0;
-  }
-
-  bool Amount::operator>=(const Amount &other) const {
-    return compareTo(other) >= 0;
-  }
-
-  std::string Amount::to_string() const {
-    cpp_dec_float_50 float50(value_);
-    float50 /= pow(10, precision_);
-    return float50.str(precision_, std::ios_base::fixed);
-  }
+  Amount::Amount(uint64_t amount) { value = amount; }
+  Amount::Amount(const Amount &) {}
+  Amount &Amount::operator=(const Amount &) { return *this; }
+  Amount::Amount(Amount &&) {}
+  Amount &Amount::operator=(Amount &&) { return *this; }
+  Amount Amount::percentage(uint64_t) const { return Amount(); }
+  Amount Amount::percentage(const Amount &) const { return Amount(); }
+  Amount Amount::operator+(const Amount &) const { return Amount(); }
+  Amount &Amount::operator+=(const Amount &) { return *this; }
+  Amount Amount::operator-(const Amount &) const { return Amount(); }
+  Amount &Amount::operator-=(const Amount &) { return *this; }
+  bool Amount::operator==(const Amount &) const { return false; }
+  bool Amount::operator!=(const Amount &) const { return false; }
+  bool Amount::operator<(const Amount &) const { return false; }
+  bool Amount::operator>(const Amount &) const { return false; }
+  bool Amount::operator<=(const Amount &) const { return false; }
+  bool Amount::operator>=(const Amount &) const { return false; }
+}
 }
--- libs/amount/amount.hpp
+++ libs/amount/amount.hpp
@@ -18,101 +18,32 @@
 #define IROHA_AMOUNT_H
 
 #include <boost/multiprecision/cpp_dec_float.hpp>
-#include <boost/multiprecision/cpp_int.hpp>
 #include <cstdint>
-#include <nonstd/optional.hpp>
 #include <string>
 
-namespace iroha {
+namespace amount {
 
-  using namespace boost::multiprecision;
-
-  /**
-   * Keeps integer and scale values allowing performing math
-   * operations on them
-   */
   class Amount {
    public:
-    /**
-     * Creates Amount with integer = 0 and scale = 0
-     */
     Amount();
+    Amount(uint64_t amount);
+    Amount(uint64_t amount, uint8_t precision);
 
-    /**
-     * Amount with integer = amount and scale = 0
-     * @param amount integer part
-     */
-    Amount(uint256_t amount);
-
-    /**
-     * Amount with provided integer and scale part
-     * @param amount integer part
-     * @param precision scale part
-     */
-    Amount(uint256_t amount, uint8_t precision);
-
-    /**
-     * Copy constructor
-     */
     Amount(const Amount&);
     Amount& operator=(const Amount&);
 
-    /**
-     * Move constructor
-     */
     Amount(Amount&&);
     Amount& operator=(Amount&&);
 
-    /**
-     * Takes percentage from current amount
-     * @param percents
-     * @return
-     */
-    Amount percentage(uint256_t percents) const;
+    Amount percentage(uint64_t) const;
+    Amount percentage(const Amount&) const;
 
-    /**
-     * Takes percentage represented as amount value
-     * The current scale and scale of percents may differ
-     * @param percents
-     * @return
-     */
-    Amount percentage(const Amount& percents) const;
-
-    friend nonstd::optional<Amount> operator+(nonstd::optional<Amount> a,
-                                              nonstd::optional<Amount> b) {
-      // check precisions
-      if (a->precision_ != b->precision_) {
-        return nonstd::nullopt;
-      }
-      auto res = a->add(*b);
-      // check overflow
-      if (res.value_ < a->value_ or res.value_ < b->value_) {
-        return nonstd::nullopt;
-      }
-      return res;
-    }
-
-    friend nonstd::optional<Amount> operator-(nonstd::optional<Amount> a,
-                                              nonstd::optional<Amount> b) {
-      // check precisions
-      if (a->precision_ != b->precision_) {
-        return nonstd::nullopt;
-      }
-      // check if a greater than b
-      if (a->value_ < b->value_) {
-        return nonstd::nullopt;
-      }
-      return a->subtract(*b);
-    }
-
+    Amount operator+(const Amount&) const;
     Amount& operator+=(const Amount&);
+
+    Amount operator-(const Amount&) const;
     Amount& operator-=(const Amount&);
 
-    /**
-     * Comparisons are possible between amounts with different precisions.
-     *
-     * @return
-     */
     bool operator==(const Amount&) const;
     bool operator!=(const Amount&) const;
     bool operator<(const Amount&) const;
@@ -124,31 +55,7 @@ namespace iroha {
     ~Amount() = default;
 
    private:
-    /**
-     * Support function for comparison operators.
-     * Returns 0 when equal, -1 when current Amount smaller, and 1 when it is
-     * greater
-     * @param other
-     * @return
-     */
-    int compareTo(const Amount& other) const;
-
-    /**
-     * Sums two amounts. Requires to have the same scale.
-     * Otherwise invalid argument exception is thrown
-     * @return
-     */
-    Amount add(const Amount&) const;
-    /**
-     * Subtracts one amount from another.
-     * Requires to have the same scale between both amounts.
-     * Otherwise invalid argument exception is thrown
-     * @return
-     */
-    Amount subtract(const Amount&) const;
-
-    boost::multiprecision::uint256_t value_;
-    uint8_t precision_;
+    boost::multiprecision::cpp_dec_float_50 value;
   };
 }
 #endif  // IROHA_AMOUNT_H
